ee8c7d12522b1183d0e248137901aec2effd8a04
chihsuan <chihsuan.tw@gmail.com>
2016-01-27 17:24:10 +0800

    update style

--- js/home.js
+++ js/home.js
@@ -1,9 +1,12 @@
 (function() {
 
+
   $(document).ready(function($) {
+    var height = $(window).height() * 0.24;
+
     $( '#my-slider' ).sliderPro({
       width: '90%',
-      height: 500
+      height: height
     });
   });
 
