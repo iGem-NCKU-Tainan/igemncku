f5ea0c20191bd6ce3670d7bd5dac6ec1ce4165a9
chihsuan <chihsuan.tw@gmail.com>
2016-01-27 11:56:02 +0800

    init

---
+++ route.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var url = require('url');
+
+route = [
+  {
+    "title": "iGem NCKU",
+    "url": "",
+    "img": "",
+    "description": "",
+    "partials": './partials.js',
+    "layout":  "./view/index.hbs",
+    "filename": "./public/index.html"
+  },
+  {
+    "title": "iGem NCKU 關於我們",
+    "url": "",
+    "img": "",
+    "description": "",
+    "partials": './partials.js',
+    "layout":  "./view/about/index.hbs",
+    "filename": "./public/about/index.html"
+  },
+  {
+    "title": "iGem NCKU 成員",
+    "url": "",
+    "img": "",
+    "description": "",
+    "partials": './partials.js',
+    "layout":  "./view/member/index.hbs",
+    "filename": "./public/memeber/index.html"
+  },
+  {
+    "title": "iGem NCKU 聯絡我們",
+    "url": "",
+    "img": "",
+    "description": "",
+    "partials": './partials.js',
+    "layout":  "./view/contact/index.hbs",
+    "filename": "./public/contact/index.html"
+  }
+]
+
+module.exports = route;
