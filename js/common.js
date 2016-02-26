0421191a466ebf074b81f1ef50c2fab48e4b0054
chihsuan <chihsuan.tw@gmail.com>
2016-02-26 17:17:32 +0800

    update navbar

--- js/common.js
+++ js/common.js
@@ -6,27 +6,27 @@
    switch(path) {
    
     case 'about':
-     $('.navbar-nav li:nth-child(2) a').addClass('active');
+     $('.navbar-nav > li:nth-child(2) a').addClass('active');
      break;
 
     case 'team':
-     $('.navbar-nav li:nth-child(3) a').addClass('active');
+     $('.navbar-nav > li:nth-child(3) .nav-text').addClass('active');
      break;
 
     case 'project':
-     $('.navbar-nav li:nth-child(4) a').addClass('active');
+     $('.navbar-nav > li:nth-child(4) a').addClass('active');
      break;
 
     case 'report':
-     $('.navbar-nav li:nth-child(5) a').addClass('active');
+     $('.navbar-nav > li:nth-child(5) a').addClass('active');
      break;
 
     case 'blog':
-     $('.navbar-nav li:nth-child(6) a').addClass('active');
+     $('.navbar-nav > li:nth-child(6) a').addClass('active');
      break;
 
     default:
-     $('.navbar-nav li:nth-child(1) a').addClass('active');
+     $('.navbar-nav > li:nth-child(1) a').addClass('active');
      break;
    
    }
