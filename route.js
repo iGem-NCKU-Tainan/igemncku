e60fb689ea37adef19fe0d3ecc400c9c3b151326
chihsuan <chihsuan.tw@gmail.com>
2016-02-26 17:39:09 +0800

    update router data

--- route.js
+++ route.js
@@ -3,50 +3,60 @@ var url = require('url');
 
 route = [
   {
-    "title": "iGem NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+      "title": "iGem NCKU",
+      "url": "",
+      "img": "",
+      "description": "",
+    },
     "partials": './partials.js',
     "layout":  "./view/index.hbs",
     "filename": "./public/index.html"
   },
   {
-    "title": "iGem NCKU 關於我們",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+      "title": "iGem NCKU About",
+      "url": "",
+      "img": "",
+      "description": "",
+    },
     "partials": './partials.js',
     "layout":  "./view/about/index.hbs",
     "filename": "./public/about/index.html"
   },
   {
-    "title": "iGem NCKU 成員",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+      "title": "iGem NCKU Team",
+      "url": "",
+      "img": "",
+      "description": "",
+    },
     "partials": './partials.js',
     "layout":  "./view/team/index.hbs",
     "filename": "./public/team/index.html"
   },
   {
-    "title": "iGem NCKU BLOG",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+      "title": "iGem NCKU BLOG",
+      "url": "",
+      "img": "",
+      "description": ""
+    },
     "partials": './partials.js',
     "layout":  "./view/blog/index.hbs",
     "filename": "./public/blog/index.html"
   },
   {
-    "title": "iGem NCKU Report",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+      "title": "iGem NCKU Report",
+      "url": "",
+      "img": "",
+      "description": ""
+    },
     "partials": './partials.js',
     "layout":  "./view/report/index.hbs",
     "filename": "./public/report/index.html"
   }
-]
+];
 
 module.exports = route;
