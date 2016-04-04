40ffed989108f477a38ffe9955f9e99a8b142bbd
xr2439 <xr2439@gmail.com>
2016-04-04 11:24:15 +0800

    update blog & fix navbar

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 (function() {
 
-  
+
    var path = window.location.pathname.split('/')[1];
    var isMobile = $(window).width() < 768;
 
    switch(path) {
-   
+
     case 'about':
      $('.navbar-nav > li:nth-child(2) a').addClass('active');
      break;
@@ -14,8 +14,12 @@
      $('.navbar-nav > li:nth-child(3) .nav-text').addClass('active');
      break;
 
+    case 'sponsor':
+     $('.navbar-nav > li:nth-child(4) a').addClass('active');
+     break;
+
     case '2016conf':
-     $('.navbar-nav > li:nth-child(4) .nav-text').addClass('active');
+     $('.navbar-nav > li:nth-child(5) .nav-text').addClass('active');
      break;
 
 //    case 'project':
@@ -23,20 +27,20 @@
 //     break;
 
     case 'report':
-     $('.navbar-nav > li:nth-child(5) a').addClass('active');
+     $('.navbar-nav > li:nth-child(6) a').addClass('active');
      break;
 
     case 'blog':
-     $('.navbar-nav > li:nth-child(6) a').addClass('active');
+     $('.navbar-nav > li:nth-child(7) a').addClass('active');
      break;
 
     default:
      $('.navbar-nav > li:nth-child(1) a').addClass('active');
      break;
-   
+
    }
-    
-  
+
+
   $(window).on('scroll', function (e){
     if ($(this).scrollTop() < 30) {
       $('#navbar').removeClass('fixed');
@@ -54,7 +58,7 @@
     }
   });
 
-  $( window ).resize(function() { 
+  $( window ).resize(function() {
     isMobile = $(window).width() < 768;
     if (!isMobile)
       $('.navbar-header').removeClass('navbar-fixed-top');
